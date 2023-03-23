8aa78c157604c6bfac101a41386409120f14c858
Yasmeen Yousef <yasmeen.yousef05@gmail.com>
2023-03-23 13:00:40 -0400

    adds delete button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit',(e) => {
+    e.preventDefault();
+    handleToDo(e.target['new-task-description'].value)
+  });
 });
+function handleToDo(todo){
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  btn.addEventListener('click',handleDelete)
+  btn.textContent = ' x '
+  p.textContent = ` ${todo} `;
+  p.appendChild(btn)
+  document.querySelector('#tasks').appendChild(p);
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
